fea3a350972d93435cac61ac85663b5d27e6f7e2
Argen <n.uulu.a@mail.ru>
2021-03-07 23:36:52 +0900

    lesson 2

--- lesson1/main.swift
+++ lesson1/main.swift
@@ -7,5 +7,48 @@
 
 import Foundation
 
-print("Hello, World!")
+//var array = [1...100]
+//
+//var grow = 0
+//
+//while grow < 100 {
+//    print(grow)
+//    grow += 1
+//}
+
+//var a = [Int] ()
+//
+//for item in 1...100 {
+//    a.append(item)
+//}
+//print(a)
+
+
+var arr = [44,23,43,2,45,54,636,65,2,14,1,52,45,464]
+//
+//for division in arr {
+//    if division % 2 == 0{
+//        print("\(division) четное")
+//    }else{
+//        print("\(division) нечетное")
+//    }
+//}
+//
+//for sorting in arr {
+//    if sorting % 3 == 0{
+//        print("\(sorting) число делится на 3 без остатка")
+//    }else{
+//        print("\(sorting) число делится на 3 c остаткoй")
+//    }
+//}
+
+//for number in arr {
+//    if number % 3 == 0 || number % 2 != 0{
+//        print(number)
+//    }
+//}
+
+//arr = arr.filter(){$0 % 3 == 0 && $0 % 2 == 0}
+//print(arr)
+
 
